2e85cdaee648b7bc7b8daf8c964b4d7346f0e254
Priyanka-247 <priyankapapu02042007@example.com>
2025-07-13 10:08:57 +0530

    Dashboard Changes

--- Project/src/App.tsx
+++ Project/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Page1 from './pages/Page1';
 import HomePage from './pages/HomePage';
+import Dashboard from './pages/DashBoard';
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const App: React.FC = () => {
       <Route path ="/buttons" element={<Page1 />} />
       <Route path ="/page1" element={<HomePage />} />
       <Route path ="/page2" element={<LoginPage />} />
+      <Route path ="/page3" element={<Dashboard />} />
     </Routes>
   );
 };
